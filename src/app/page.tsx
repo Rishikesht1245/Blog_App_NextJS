dec0abfc4914ce6f0dad4ac96f7873ea5dcf3630
Rishikesh T <rishikeshtgit@gmail.com>
2024-01-31 16:25:22 +0530

    Tracked : Necessary pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h3 className="text-red-400">Hello world!</h3>
     </main>
   );
